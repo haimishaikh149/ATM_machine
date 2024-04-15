b06887e6d3ba84b5d4b7c67ef89d17877b572e54
haimishaikh149 <160590123+haimishaikh149@users.noreply.github.com>
2024-04-15 19:44:49 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,51 @@
+import inquirer from "inquirer";
+
+let mybalance = 10000; // doller
+let mypin = 1234;
+ let message = "your balnace is";
+// mybalance -= 6000
+
+console.log(mybalance+ message )
+
+// let pinAnswer = await inquirer.prompt(
+//     [
+//         {
+//             name: "pin",
+//             message: "enter your pin",
+//             type: "number",
+//         }
+//     ]
+// );
+// // 12345 === 1234 - false 
+// if (pinAnswer.pin === mypin){
+//     console.log("correct pin code!!");
+
+//     let operationAns = await inquirer.prompt(
+//         [
+//             {
+//                 name: "operation",
+//                 message: "please select option",
+//                 type: "list",
+//                 choices: ["withdraw","check balance"]
+//             }
+//         ]
+//     )
+//     console.log(operationAns);
+
+//  if (operationAns.operation === "withdraw"){
+//     let amountAns = await inquirer.prompt(
+//         [
+//             {
+//                 name: "amount",
+//                 message: "enter your amount",
+//                 type: "number"
+//             }
+//         ]
+//     );
+
+//     mybalance -= amountAns.amount
+
+//     console.log("your remaing balance:" + mybalance)
+//  }
+
+// }
